1552264340ac5f8931a5dd9a181248579893aa95
Linh Ha <143023730+linhha930@users.noreply.github.com>
2026-04-19 11:42:35 -0500

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dict = {}
+        result = []
+
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+
+            if sorted_word not in dict:
+                dict[sorted_word] = []
+            
+            dict[sorted_word].append(word)
+        
+        for val in dict.values():
+            result.append(val)
+        
+        return result
