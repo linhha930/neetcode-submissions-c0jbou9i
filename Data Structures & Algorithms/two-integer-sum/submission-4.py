ea8e5bb1abc7537cc36c101a67808c2514798fcd
Linh Ha <143023730+linhha930@users.noreply.github.com>
2026-04-19 10:50:49 -0500

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # if len(nums) == 2:
+        #     return [0, 1]
+
+        dict = {}
+
+        for i, val in enumerate(nums): 
+            remainder = target - val
+
+            if remainder in dict:
+                return [dict[remainder], i]
+            else:
+                dict[val] = i
